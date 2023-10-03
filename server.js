be1d2926b400ad6d4c403deb1cdcc9dc7b5465a7
Arkish Shah <shah.arkish22@gmail.com>
2023-10-03 08:04:09 -0700

    Updated server.js for Heroku config

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const { root } = require('./src/resolvers');
 const schema = require('./src/schema');
 const app = express();
 
-app.use('/transactions', graphqlHTTP({
+app.use('/', graphqlHTTP({
     schema: schema,
     rootValue: root,
     graphiql: true, // You can turn this off in production
 }));
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000; // Use the PORT Heroku sets or 4000 as a fallback.
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
